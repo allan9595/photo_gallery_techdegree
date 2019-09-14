e69c59c2a21f8c2b402edca64262e52e6159226b
*allan9595* <bohanzhang@my.unt.edu>
2019-09-13 23:41:09 -0400

    add reponsive layout for search box

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
 $('.input').on('keyup', (e) => {
     //target the caption field
     e.preventDefault();
-    //console.log(e.target.value);
-    //console.log($('a[data-caption]'))
+    
     
     //pass each search to the search func
     $('a[data-caption]').each((index, element) => {
+        //based on the caption, return the related image
         if($(element).attr('data-caption').includes(e.target.value.toLowerCase())){
             $($('.img-avatar')[index]).show();
         }else{
             $($('.img-avatar')[index]).hide();
         }
     })
-    //based on the caption, return the related image
+    
 })
